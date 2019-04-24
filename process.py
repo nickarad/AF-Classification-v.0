6f19e0590e7dcc25a508204ca9a5573e2489b9f7
nickarad <nickarad94@gmail.com>
2019-04-24 15:35:36 +0300

    Filter Dataset

--- process.py
+++ process.py
@@ -7,13 +7,28 @@ from biosppy.signals import ecg
 
 x = np.load('x_data.npy')
 y = np.load('y_data.npy')
-ecg_signal = x[0]
-print(ecg_signal)
-out = ecg.ecg(signal=ecg_signal, sampling_rate=100, show=False)
-# print(out)
-filtered = out["filtered"]
-print(filtered)
-plt.plot(filtered,linewidth=2, alpha=2.0)
+x = x[0:100]
+x_filt = np.array([])
+for ecg_signal in x:
+    # ecg_signal = r
+    out = ecg.ecg(signal=ecg_signal, sampling_rate=300, show=False)
+    filtered = out["filtered"]
+    x_filt = np.append(x_filt,filtered)
+    # print(filtered)
+x_filt = x_filt.reshape(-1, 2714)
+np.save('x_filtered.npy', x_filt)
+print(x_filt.shape)
+print(x_filt)
 
-plt.grid(color='r', linestyle='--', linewidth=0.3)
-plt.show()
+# ===================================================
+
+
+# ecg_signal = x[0]
+# print(ecg_signal)
+# out = ecg.ecg(signal=ecg_signal, sampling_rate=300, show=False)
+# # print(out)
+# filtered = out["filtered"]
+# print(filtered)
+# plt.plot(filtered,linewidth=2, alpha=2.0)
+# plt.grid(color='r', linestyle='--', linewidth=0.3)
+# plt.show()
